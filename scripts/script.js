33d54fb358e6bf2bb1fceb46768ae560a6419059
Kentson Tran <kttran1@go.olemiss.edu>
2023-01-17 19:06:16 -0600

    new entered letter system; formatting

--- scripts/script.js
+++ scripts/script.js
@@ -11,13 +11,18 @@ let enteredLetters = new Set();
 let guesses = 0;
 let inPlay = false;
 
+const lettersArr = [
+                        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
+                        ['i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q'],
+                        ['r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+                    ];
 
 const word_rr = document.getElementById('word-rr');
 const word_space = document.getElementById('word-space');
 word_rr.addEventListener("click", reroll);
 
 const inputBox = document.getElementById('input-box');
-const enteredLettersBox = document.getElementById('entered-letters');
+const enteredLettersCont = document.getElementById('entered-letters-cont');
 inputBox.addEventListener('input', enterLetter);
 
 const statusBox = document.getElementById('status-box');
@@ -44,7 +49,7 @@ function enterLetter(e){
             else{
                 enteredLetters.add(e.target.value.toLowerCase());
                 enteredLettersStr += e.target.value.toLowerCase();
-                enteredLettersBox.textContent = enteredLettersStr;
+                //enteredLettersBox.textContent = enteredLettersStr;
                 fillDisplay(e.target.value.toLowerCase());
             }
         }
@@ -75,31 +80,42 @@ function reroll(){
 }
 
 function fillDisplay(letter){
+    let inputLetter = document.getElementById(letter);
     if(currWord.match(letter)){
+        inputLetter.classList.add('letter-correct');
         for(let i = 0; i < currWord.length; i++){
             if(currWord.charAt(i) === letter){
                 wordDisplay = setCharAt(wordDisplay, i, letter);
-                updateWordDisplay();
+                word_space.textContent=wordDisplay;
             }
         }
         console.log("word contains " + letter);
     }
     else{
+        inputLetter.classList.add('letter-incorrect');
         guesses += 1;
         if (guesses >= 8){
             inPlay = false;
             displayInput();
             mainImg.src = "./img/guess_final.jpg";
-            statusBox.textContent = "out of LIVES!\nthe word is " + currWord;
+            let tempElem = document.createElement('p');
+            tempElem.textContent = "out of LIVES! the word is"
+            statusBox.appendChild(tempElem);
+            tempElem = document.createElement('p');
+            tempElem.textContent = currWord;
+            tempElem.setAttribute('id', 'word-reveal');
+            statusBox.append(tempElem);
         }
         else{
-            displayLifeImg();
+            mainImg.src = "./img/guess_" + guesses + ".jpg";
         }
         
         console.log('no match');
     }
     if (wordDisplay === currWord){
-        statusBox.textContent = "congrats u got the word!";
+        let tempElem = document.createElement('p');
+        tempElem.textContent = "congratz u got the word!"
+        statusBox.appendChild(tempElem);
         inPlay = false;
         displayInput();
     }
@@ -110,15 +126,12 @@ function setCharAt(str,index,chr){
     return str.substring(0,index) + chr + str.substring(index+1);
 }
 
-function updateWordDisplay(){
-    word_space.textContent=wordDisplay;
-}
-
 function reset(){
     const emptySet = new Set();
     enteredLetters = emptySet;
     enteredLettersStr = "";
-    enteredLettersBox.textContent = enteredLettersStr;
+    enteredLettersCont.textContent = '';
+    generateEnteredLettersCont();
     statusBox.textContent = "";
     guesses = 0;
     mainImg.src = "./img/guess_0.jpg";
@@ -129,6 +142,17 @@ function displayInput(){
     else inputBox.style.visibility = "hidden";
 }
 
-function displayLifeImg(){
-    mainImg.src = "./img/guess_" + guesses + ".jpg";
+function generateEnteredLettersCont(){
+    for (let i = 0; i < lettersArr.length; i++){
+        let letterRow = document.createElement('div');
+        letterRow.classList.add('letter-row-cont');
+        for (let j = 0 ; j < lettersArr[i].length; j++){
+            let tempLetter = document.createElement('span');
+            tempLetter.classList.add('letter');
+            tempLetter.setAttribute('id', lettersArr[i][j]);
+            tempLetter.textContent = lettersArr[i][j].toUpperCase();
+            letterRow.appendChild(tempLetter);
+        }
+        enteredLettersCont.appendChild(letterRow);
+    }
 }
